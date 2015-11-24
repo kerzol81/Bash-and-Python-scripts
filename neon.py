5222b9497e78cc8c824eee9183396977f6033e32
Z <kerenyi.nagy.zoltan@gmail.com>
2015-11-24 06:18:14 +0100

    Create neon.py

---
+++ neon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pthon
+
+import time
+import os
+
+
+def	neon(s):
+  x = 0
+  while x <= len(s):
+  os.system("clear")
+  print s[:x]
+  x = x + 1
+  time.sleep(0.1)
+
+s = 'hello world'
+
+neon(s)
