dd897360199a101915bbeb81cf78f06985041ccd
Z <kerenyi.nagy.zoltan@gmail.com>
2017-04-23 15:39:45 +0200

    Update leadingZeroInsert.py

--- leadingZeroInsert.py
+++ leadingZeroInsert.py
@@ -1,11 +1,12 @@
 import os
 
-folder = "Scott.Ross_Goldberg.Variations"
-
+folder = "/home/zoli/PycharmProjects/leadingZeros/Scott.Ross_Goldberg.Variations/"
+os.chdir(folder)
 
 for i in os.listdir(folder):
     filename, extension = (os.path.splitext(i))
     filenumber = (filename.split('.')[0].zfill(2))
-    filename = (filename.split('. ')[1])
+    filename = (filename.split('.')[1])
     new_filename =('{}.{}{}'.format(filenumber, filename, extension))
+    #print(new_filename)
     os.rename(i, new_filename)
