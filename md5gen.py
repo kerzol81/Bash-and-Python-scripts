5340608e36a48c2be284b831a4ba8c13d1a124ac
Z <kerenyi.nagy.zoltan@gmail.com>
2017-06-06 13:50:43 +0200

    Add files via upload

---
+++ md5gen.py
@@ -0,0 +1,15 @@
+from checksumdir import dirhash
+import time
+
+dir = 'C:\\Users\\zoli\\Desktop\\Akademia2'
+
+print('[+] Calculating MD5 hash on : ' + dir + ' directory, started at: ' + str(time.strftime("%Y-%m-%d %H:%M:%S")))
+
+md5hash = dirhash(dir, 'md5')
+
+message = '[+] Directory: ' + dir + ' \n[+] MD5 hash: ' + md5hash[:8] + ' ' + md5hash[8:16] + ' ' + md5hash[16:24] + ' ' + md5hash[24:] \
+          + ' \n[+] Creation time: ' + str(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+print('[+] Done!')
+print(message)
+
