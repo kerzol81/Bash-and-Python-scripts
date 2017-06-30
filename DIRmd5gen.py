c03c510525de9c7f7d29482f5b73e2a04c71c690
Z <kerenyi.nagy.zoltan@gmail.com>
2017-06-30 17:42:53 +0200

    Update DIRmd5gen.py

--- DIRmd5gen.py
+++ DIRmd5gen.py
@@ -17,11 +17,3 @@ def md5(dir):
     print(md5hash)
     createfile(dir, md5hash)
 
-def sha256(dir):
-    print('[+] Calculating SHA256 hash on : ' + dir + ' directory, started at: ' + currenttime())
-    sha256hash = dirhash(dir, 'sha256')
-    print('[+] SHA256 hash: ' + sha256hash[:8] + ' ' + sha256hash[8:16] + ' ' + sha256hash[16:24] + ' ' +
-          sha256hash[24:32] + ' ' + sha256hash[32:40] + ' ' + sha256hash[40:48] + ' ' + sha256hash[48:56] +
-          ' ' + sha256hash[56:64])
-
-
