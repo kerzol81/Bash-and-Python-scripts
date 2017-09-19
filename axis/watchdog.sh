54b8234bd72a3ad2bf22031e4ab560a7f07d2e37
Z <kerenyi.nagy.zoltan@gmail.com>
2017-09-19 12:00:30 +0200

    Update watchdog.sh

--- axis/watchdog.sh
+++ axis/watchdog.sh
@@ -14,9 +14,6 @@ TO=$6
 FROM=$7
 EMAIL_USERNAME=$8
 
-# script settings:
-
-
 function sendRestartMessage(){
 	local MESSAGE="status: $AXIS_IP disconnected, restart $ID"
 	swaks --to "$TO" --from "$FROM" --server "$SMTP_SERVER" --header "Subject: $ID disconnected" --body "$MESSAGE" >/dev/null
@@ -31,16 +28,6 @@ function writeToSyslog(){
 	# writes to syslog
 	logger "$ID" "is" "$STATE" 
 	}
-
-function parseSyslog(){
-	# searching for the sring "is disconnected" in last 60 minutes of syslog file
-	disconnected=$(awk "/^$(date --date="-60 min" "+%b %_d %H:%M")/{p++} p" /var/log/syslog | grep -c "$ID is disconnected")
-	
-	if [ "$disconnected" -gt 3 ];then
-		echo "sending email"
-	
-	fi
-}
 
 function queryAPI(){
 	curl -m 3 -X GET http://"$USERNAME":"$PASSWORD"@"$AXIS_IP"/axis-cgi/videostatus.cgi?status=1,2,3,4 >/dev/null
