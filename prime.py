390dbe161b0ca3264a0e2f87722557fafe9a3b7c
Z <kerenyi.nagy.zoltan@gmail.com>
2017-10-08 09:33:16 +0200

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def isPrime(num):
     try:
         assert isinstance(num, int) and num > 0
@@ -11,8 +12,12 @@ def isPrime(num):
     if num == 0 or num == 1:
         print('[-] 1 or 0 ar not primes')
 
+    elif num == 2:
+        print('[*] ' + str(num) + ' is prime')
+
     else:
-        for a in range(2, int(num ** 0.5)):
+
+        for a in range(3, int(num ** 0.5), 2):
 
             if num % a == 0:
                 print('[-] ' + str(num) + ' is not prime')
