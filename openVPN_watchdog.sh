ae53393c314eb71c63a3ba7e8849f0a6396a7f08
Z <kerenyi.nagy.zoltan@gmail.com>
2019-05-29 08:09:59 +0200

    Create openVPN_watchdog.sh

---
+++ openVPN_watchdog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# OpenVPN watchdog 
+# Start it with cron @reboot
+
+ISLANDS=( 10 20 30 40 50 60 )
+WAIT=300
+
+while true;
+do
+	for i in "${ISLANDS[@]}";do
+		STATUS=$(service openvpn@$i status | grep -o "inactive")
+
+        	if [ "$STATUS" == "inactive" ];then
+                	systemctl stop openvpn@$i.service
+                	sleep 5
+                	systemctl start openvpn@$i.service
+                	logger "Watchdog: $i island has been restarted"
+        	fi
+	done
+
+sleep "$WAIT"
+
+done
