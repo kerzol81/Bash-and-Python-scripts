013c1b4fa785162f7cfff9655de7b8fa1e5a3b38
Z <kerenyi.nagy.zoltan@gmail.com>
2020-08-23 12:02:33 +0200

    Create doorCheck.py

---
+++ axis/doorCheck.py
@@ -0,0 +1,26 @@
+import requests
+import time
+import subprocess
+
+
+def check_door():
+    url = "http://192.168.0.90/axis-cgi/io/port.cgi"
+
+    querystring = {"check": "1"}
+
+    payload = ""
+    headers = {'authorization': 'Basic cm9vdDp6Z2FyZGVuMjAxOQ=='}
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    return int(response.text.split("=")[1])
+
+
+while True:
+    state = check_door()
+    if state is 0:
+        print("open")
+        subprocess.call(['notify-send', 'App Title', 'Message'])
+
+
+    time.sleep(5)
