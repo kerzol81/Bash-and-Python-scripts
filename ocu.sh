a1bc4d0c9e60760586d3f0a970cf7cc2360b15f7
Z <kerenyi.nagy.zoltan@gmail.com>
2020-10-27 22:53:44 +0100

    Update ocu.sh

--- ocu.sh
+++ ocu.sh
@@ -6,7 +6,7 @@ i='/xxx/xxxxxxx/xxxxx/yyyyyyyyyyyyyyyyyyyyyyy.zzz'
 
 FILENAME=$(echo $i | awk -F '/' '{print $NF}')
 
-if echo "$filename" | egrep '^Rec[0-9]_[0-9]{8}_[0-9]{6}' >/dev/null
+if echo "$FILENAME" | egrep '^Rec[0-9]_[0-9]{8}_[0-9]{6}' >/dev/null
 then
     DAY=$(echo $i | awk -F '/' '{print substr($NF,6,4)"-"substr($NF,10,2)"-"substr($NF,12,2)}')
     mkdir -p "$DAY"
