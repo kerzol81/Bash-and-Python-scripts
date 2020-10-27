719b9d322ea2f5f6b693458a21fd087ad45ea4fe
Z <kerenyi.nagy.zoltan@gmail.com>
2020-10-27 22:52:48 +0100

    Create ocu.sh

---
+++ ocu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# test !!!
+
+i='/xxx/xxxxxxx/xxxxx/yyyyyyyyyyyyyyyyyyyyyyy.zzz' 
+
+FILENAME=$(echo $i | awk -F '/' '{print $NF}')
+
+if echo "$filename" | egrep '^Rec[0-9]_[0-9]{8}_[0-9]{6}' >/dev/null
+then
+    DAY=$(echo $i | awk -F '/' '{print substr($NF,6,4)"-"substr($NF,10,2)"-"substr($NF,12,2)}')
+    mkdir -p "$DAY"
+    mv $FILENAME $DAY
+fi
