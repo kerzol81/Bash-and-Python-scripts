4251a6aafd6cdb11b2e7206d8e9362de6ee54436
Z <kerenyi.nagy.zoltan@gmail.com>
2021-09-27 20:25:45 +0200

    Create gnuRadioConference2021.sh

---
+++ gnuRadioConference2021.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# monday
+youtube-dl https://www.youtube.com/watch?v=Qx8uFhumgGM
+
+# tuesday
+youtube-dl https://www.youtube.com/watch?v=ef30VPIx-LM
+
+# wednesday
+youtube-dl https://www.youtube.com/watch?v=2s940fttDco
+
+# thursday
+youtube-dl https://www.youtube.com/watch?v=BaRgx5ehdOw
